5313ca5e8682370d568000a1d5ad2f5cd78eeabf
Armin <61236723+dastrinken@users.noreply.github.com>
2023-05-13 16:58:50 +0200

    Bugfix
    Riesen sorry.

--- main/metro.config.js
+++ main/metro.config.js
@@ -7,16 +7,11 @@
 
 module.exports = {
   transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
     }),
-  },
-  resolver: {
-    assetExts: assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
   },
 };

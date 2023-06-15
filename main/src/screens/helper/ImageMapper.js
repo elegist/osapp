e0dabe6ddbd970084cd8b798501927fefb56bf24
Armin <61236723+dastrinken@users.noreply.github.com>
2023-06-15 15:43:05 +0200

    First contents for cs tasks

--- main/src/screens/helper/ImageMapper.js
+++ main/src/screens/helper/ImageMapper.js
@@ -3,27 +3,34 @@
  */
 class ImageMapper {
   static getImagePath(imageName) {
+    const path = '../../assets/osa_images/';
     switch (imageName) {
       case 'thm-haupteingang.jpg':
-        return require('../../assets/osa_images/thm-haupteingang.jpg');
+        return require(path + 'thm-haupteingang.jpg');
       case 'thm-campus.jpg':
-        return require('../../assets/osa_images/thm-campus.jpg');
+        return require(path + 'thm-campus.jpg');
       case 'thm-gebaeude-b.jpg':
-        return require('../../assets/osa_images/thm-gebaeude-b.jpg');
+        return require(path + 'thm-gebaeude-b.jpg');
       case 'thm-hoersaal.jpg':
-        return require('../../assets/osa_images/thm-hoersaal.jpg');
+        return require(path + 'thm-hoersaal.jpg');
       case 'thm-info.jpg':
-        return require('../../assets/osa_images/thm-info.jpg');
+        return require(path + 'thm-info.jpg');
       case 'thm-medienlabor.jpg':
-        return require('../../assets/osa_images/thm-medienlabor.jpg');
+        return require(path + 'thm-medienlabor.jpg');
       case 'thm-studio.jpg':
-        return require('../../assets/osa_images/thm-studio.jpg');
+        return require(path + 'thm-studio.jpg');
       case 'thm-mensa.jpg':
-        return require('../../assets/osa_images/thm-mensa.jpg');
+        return require(path + 'thm-mensa.jpg');
       case 'thm-bib.jpg':
-        return require('../../assets/osa_images/thm-bib.jpg');
+        return require(path + 'thm-bib.jpg');
       case 'thm-bib2.jpg':
-        return require('../../assets/osa_images/thm-bib2.jpg');
+        return require(path + 'thm-bib2.jpg');
+      case 'informatik.jpg':
+        return require(path + 'informatik.jpg');
+      case 'internet.jpg':
+        return require(path + 'internet.jpg');
+      case 'programming.jpg':
+        return require(path + 'programming.jpg');
       // Add more cases for other image names
       default:
         return null; // Handle unknown image names

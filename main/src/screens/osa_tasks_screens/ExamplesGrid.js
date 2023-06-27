207b70a98830fc5c2dcb8cd6f697165ba5c44c58
Robin Patzak <robin.patzak@proton.me>
2023-06-27 16:46:31 +0200

    setup modal basic modal for the examples

--- main/src/screens/osa_tasks_screens/ExamplesGrid.js
+++ main/src/screens/osa_tasks_screens/ExamplesGrid.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -6,12 +6,32 @@ import {
   Image,
   Text,
   ScrollView,
+  Modal,
 } from 'react-native';
 import globalStyles from '../../styles/GlobalStyleSheet';
 import ImageMapper from '../helper/ImageMapper';
 import examplesData from '../../data/examplesData.json';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import ImageGallery from '../../components/ImageGallery';
 
 class ExamplesGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+      modalContent: null,
+    };
+  }
+
+  openModalWithContent = content => {
+    this.setState({modalVisible: true});
+    this.setState({modalContent: content});
+  };
+
+  closeModal = () => {
+    this.setState({modalVisible: false});
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -24,10 +44,7 @@ class ExamplesGrid extends Component {
                   {topic.examples.map((example, index) => (
                     <TouchableOpacity
                       key={index}
-                      onPress={() => {
-                        console.log(example.title);
-                        // Add functionality here
-                      }}
+                      onPress={() => this.openModalWithContent(example)}
                       style={styles.thumbnailContainer}>
                       <Image
                         source={ImageMapper.getImagePath(example.thumbnail)}
@@ -48,10 +65,65 @@ class ExamplesGrid extends Component {
             );
           })}
         </ScrollView>
+
+        {this.state.modalContent && (
+          <ExampleModal
+            modalVisible={this.state.modalVisible}
+            onRequestClose={this.closeModal}
+            content={this.state.modalContent}
+          />
+        )}
       </View>
     );
   }
 }
+
+const ExampleModal = ({modalVisible, onRequestClose, content}) => {
+  const styles = StyleSheet.create({
+    closeButton: {
+      position: 'absolute',
+      top: -35,
+      right: -35,
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      margin: 5,
+      elevation: 5,
+      backgroundColor: '#dd4040',
+      borderRadius: 50,
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0,0,0,0.75)',
+    },
+    modalContent: {
+      height: '90%',
+      width: '70%',
+      backgroundColor: 'white',
+      padding: 20,
+      borderRadius: 10,
+      gap: 20,
+    },
+  });
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={onRequestClose}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <TouchableOpacity style={styles.closeButton} onPress={onRequestClose}>
+            <Icon name="close" size={32} color="white"></Icon>
+          </TouchableOpacity>
+          <ImageGallery />
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {

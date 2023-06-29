b60ada4a47a8a9a69906b4d9ebfd6c4660b767d9
Robin Patzak <robin.patzak@proton.me>
2023-06-29 17:49:37 +0200

    put video controls on top of video and made them disappear after 3 seconds

--- main/src/components/VideoPlayer.js
+++ main/src/components/VideoPlayer.js
@@ -1,5 +1,5 @@
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import React, {useRef, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Animated} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import Video from 'react-native-video';
 import Slider from '@react-native-community/slider';
 import ImageMapper from '../screens/helper/ImageMapper';
@@ -14,6 +14,21 @@ const VideoPlayer = ({video, thumbnail}) => {
   const [volume, setVolume] = useState(0.7);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [controlsVisible, setControlsVisible] = useState(true);
+
+  const controlsTimeout = 3000;
+  const controlsOpacity = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    if (!paused) {
+      const timer = setTimeout(() => {
+        setControlsVisible(false);
+        fadeOutControls();
+      }, controlsTimeout);
+
+      return () => clearTimeout(timer);
+    }
+  }, [paused, controlsVisible]);
 
   const handlePlayPause = () => {
     setPaused(!paused);
@@ -37,30 +52,29 @@ const VideoPlayer = ({video, thumbnail}) => {
     setDuration(data.playableDuration);
   };
 
+  const handleVideoPress = () => {
+    setControlsVisible(true);
+    fadeInControls();
+  };
+
+  const fadeOutControls = () => {
+    Animated.timing(controlsOpacity, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeInControls = () => {
+    Animated.timing(controlsOpacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.controlsContainer}>
-        <TouchableOpacity style={{width: '14%'}} onPress={handleMuteUnmute}>
-          <Icon
-            name={muted ? 'volume-off' : 'volume-up'}
-            size={getResponsiveSizing(36)}
-            color="black"
-          />
-        </TouchableOpacity>
-        <Slider
-          value={volume}
-          minimumValue={0}
-          maximumValue={1}
-          step={0.01}
-          onValueChange={handleVolumeChange}
-          vertical={true}
-          style={styles.slider}
-          minimumTrackTintColor="#8CBA45"
-          maximumTrackTintColor="#bababa"
-          thumbTintColor="#8CBA45"
-        />
-      </View>
-
       {paused ? (
         <FastImage
           source={ImageMapper.getImagePath(thumbnail)}
@@ -70,43 +84,79 @@ const VideoPlayer = ({video, thumbnail}) => {
       ) : (
         <Video
           ref={videoRef}
+          style={styles.video}
           source={ImageMapper.getImagePath(video)}
           paused={paused}
           muted={muted}
           volume={volume}
           onProgress={handleProgress}
+          onTouchStart={handleVideoPress}
           resizeMode="contain"
-          style={styles.video}
         />
       )}
 
-      <View style={styles.controlsContainer}>
-        <TouchableOpacity onPress={handlePlayPause}>
-          <Icon
-            name={paused ? 'play-circle-o' : 'pause-circle-o'}
-            size={getResponsiveSizing(40)}
-            color="black"
-          />
-        </TouchableOpacity>
-
-        <Slider
-          value={currentTime}
-          minimumValue={0}
-          maximumValue={duration}
-          step={0.01}
-          onValueChange={handleSeek}
-          style={styles.slider}
-          minimumTrackTintColor="#8CBA45"
-          maximumTrackTintColor="#bababa"
-          thumbTintColor="#8CBA45"
-        />
-      </View>
+      {controlsVisible && (
+        <View
+          style={{
+            width: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+          }}>
+          <Animated.View
+            style={[styles.controlsContainer, {opacity: controlsOpacity}]}>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                style={{width: '14%'}}
+                onPress={handleMuteUnmute}>
+                <Icon
+                  name={muted ? 'volume-off' : 'volume-up'}
+                  size={getResponsiveSizing(36)}
+                  color="white"
+                />
+              </TouchableOpacity>
+              <Slider
+                style={styles.slider}
+                value={volume}
+                minimumValue={0}
+                maximumValue={1}
+                step={0.01}
+                onValueChange={handleVolumeChange}
+                minimumTrackTintColor="#8CBA45"
+                maximumTrackTintColor="#bababa"
+                thumbTintColor="#8CBA45"
+              />
+            </View>
+            <TouchableOpacity onPress={handlePlayPause}>
+              <Icon
+                name={paused ? 'play-circle' : 'pause-circle'}
+                size={getResponsiveSizing(56)}
+                color="white"
+              />
+            </TouchableOpacity>
+            <Slider
+              style={styles.slider}
+              value={currentTime}
+              minimumValue={0}
+              maximumValue={duration}
+              step={0.01}
+              onValueChange={handleSeek}
+              minimumTrackTintColor="#8CBA45"
+              maximumTrackTintColor="#bababa"
+              thumbTintColor="#8CBA45"
+            />
+          </Animated.View>
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -115,12 +165,10 @@ const styles = StyleSheet.create({
     height: getResponsiveSizing(90 * 1.5),
   },
   controlsContainer: {
+    height: '100%',
     width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: 'white',
   },
   slider: {
     width: '70%',

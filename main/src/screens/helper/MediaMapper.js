532e6ba3684e7160714b347bc283ac52c0d8fcb6
Robin Patzak <robin.patzak@proton.me>
2023-07-02 18:11:06 +0200

    added all necessary media files

--- main/src/screens/helper/MediaMapper.js
+++ main/src/screens/helper/MediaMapper.js
@@ -5,7 +5,8 @@ class MediaMapper {
   static getMediaPath(mediaName) {
     const path = '../../assets/osa_images/';
     const thumbnailPath = '../../assets/project_examples/thumbnails/';
-    const contentPath = '../../assets/project_examples/content/';
+    const contentImagesPath = '../../assets/project_examples/content/imgs/';
+    const contentVideosPath = '../../assets/project_examples/content/videos/';
     switch (mediaName) {
       case 'thm-haupteingang.jpg':
         return require(path + 'thm-haupteingang.jpg');
@@ -33,32 +34,96 @@ class MediaMapper {
         return require(path + 'internet.jpg');
       case 'programming.jpg':
         return require(path + 'programming.jpg');
-      case 'thumb1.jpg':
-        return require(thumbnailPath + 'thumb1.jpg');
-      case 'thumb2.jpg':
-        return require(thumbnailPath + 'thumb2.jpg');
-      case 'thumb3.jpg':
-        return require(thumbnailPath + 'thumb3.jpg');
-      case 'medienproduktion.jpg':
-        return require(thumbnailPath + 'medienproduktion.jpg');
-      case 'webframeworks.jpg':
-        return require(thumbnailPath + 'webframeworks.jpg');
-      case 'frontend.jpg':
-        return require(thumbnailPath + 'frontend.jpg');
-      case 'mow2.jpg':
-        return require(thumbnailPath + 'mow2.jpg');
-      case 'av2-1.jpg':
-        return require(thumbnailPath + 'av2-1.jpg');
-      case 'av2-2.jpg':
-        return require(thumbnailPath + 'av2-2.jpg');
-      case 'foto.jpg':
-        return require(thumbnailPath + 'foto.jpg');
-      case 'angels_720p.mp4':
-        return require(contentPath + 'angels_720p.mp4');
-      case 'mow2.mp4':
-        return require(contentPath + 'mow2.mp4');
-      case 'lazarus_720p.mp4':
-        return require(contentPath + 'lazarus_720p.mp4');
+      case 'av2-angels-thumbnail.png':
+        return require(thumbnailPath + 'av2-angels-thumbnail.png');
+      case 'av2-lazarus-thumbnail.png':
+        return require(thumbnailPath + 'av2-lazarus-thumbnail.png');
+      case 'fd-dijkstra-thumbnail.png':
+        return require(thumbnailPath + 'fd-dijkstra-thumbnail.png');
+      case 'fub-regeneration-thumbnail.png':
+        return require(thumbnailPath + 'fub-regeneration-thumbnail.png');
+      case 'gd1-car-thumbnail.png':
+        return require(thumbnailPath + 'gd1-car-thumbnail.png');
+      case 'gd2-lamp-thumbnail.png':
+        return require(thumbnailPath + 'gd2-lamp-thumbnail.png');
+      case 'gd2-ring-thumbnail.png':
+        return require(thumbnailPath + 'gd2-ring-thumbnail.png');
+      case 'gd3-mimic-thumbnail.png':
+        return require(thumbnailPath + 'gd3-mimic-thumbnail.png');
+      case 'gd3-rack-thumbnail.png':
+        return require(thumbnailPath + 'gd3-rack-thumbnail.png');
+      case 'md-iphone-thumbnail.png':
+        return require(thumbnailPath + 'md-iphone-thumbnail.png');
+      case 'md-squirrel-thumbnail.png':
+        return require(thumbnailPath + 'md-squirrel-thumbnail.png');
+      case 'mow2-bug-thumbnail.png':
+        return require(thumbnailPath + 'mow2-bug-thumbnail.png');
+      case 'mpm-reise-thumbnail.png':
+        return require(thumbnailPath + 'mpm-reise-thumbnail.png');
+      case 'wfr-tasse-thumbnail.png':
+        return require(thumbnailPath + 'wfr-tasse-thumbnail.png');
+      case 'fd-dijkstra.png':
+        return require(contentImagesPath + 'fd-dijkstra.png');
+      case 'fub-regeneration.png':
+        return require(contentImagesPath + 'fub-regeneration.png');
+      case 'gd1-detail-1.png':
+        return require(contentImagesPath + 'gd1-detail-1.png');
+      case 'gd1-detail-2.png':
+        return require(contentImagesPath + 'gd1-detail-2.png');
+      case 'gd1-full.png':
+        return require(contentImagesPath + 'gd1-full.png');
+      case 'gd1-uv.png':
+        return require(contentImagesPath + 'gd1-uv.png');
+      case 'gd1-wireframe.png':
+        return require(contentImagesPath + 'gd1-wireframe.png');
+      case 'gd2-lamp.png':
+        return require(contentImagesPath + 'gd2-lamp.png');
+      case 'gd2-ring.png':
+        return require(contentImagesPath + 'gd2-ring.png');
+      case 'mow2-game.png':
+        return require(contentImagesPath + 'mow2-game.png');
+      case 'mow2-gameover.png':
+        return require(contentImagesPath + 'mow2-gameover.png');
+      case 'mow2-mainmenu.png':
+        return require(contentImagesPath + 'mow2-mainmenu.png');
+      case 'mow2-settings.png':
+        return require(contentImagesPath + 'mow2-settings.png');
+      case 'mpm-reise-1.png':
+        return require(contentImagesPath + 'mpm-reise-1.png');
+      case 'mpm-reise-2.png':
+        return require(contentImagesPath + 'mpm-reise-2.png');
+      case 'mpm-reise-3.png':
+        return require(contentImagesPath + 'mpm-reise-3.png');
+      case 'wfr-1.png':
+        return require(contentImagesPath + 'wfr-1.png');
+      case 'wfr-2.png':
+        return require(contentImagesPath + 'wfr-2.png');
+      case 'wfr-3.png':
+        return require(contentImagesPath + 'wfr-3.png');
+      case 'wfr-4.png':
+        return require(contentImagesPath + 'wfr-4.png');
+      case 'wfr-5.png':
+        return require(contentImagesPath + 'wfr-5.png');
+      case 'av2-angels.mp4':
+        return require(contentVideosPath + 'av2-angels.mp4');
+      case 'av2-lazarus.mp4':
+        return require(contentVideosPath + 'av2-lazarus.mp4');
+      case 'fd-algorithm.mp4':
+        return require(contentVideosPath + 'fd-algorithm.mp4');
+      case 'fd-story.mp4':
+        return require(contentVideosPath + 'fd-story.mp4');
+      case 'gd3-mimic.mp4':
+        return require(contentVideosPath + 'gd3-mimic.mp4');
+      case 'gd3-rack.mp4':
+        return require(contentVideosPath + 'gd3-rack.mp4');
+      case 'md-iphone.mp4':
+        return require(contentVideosPath + 'md-iphone.mp4');
+      case 'md-squirrel.mp4':
+        return require(contentVideosPath + 'md-squirrel.mp4');
+      case 'mow2-highlevel.mp4':
+        return require(contentVideosPath + 'mow2-highlevel.mp4');
+      case 'mow2-start.mp4':
+        return require(contentVideosPath + 'mow2-start.mp4');
       // Add more cases for other image names
       default:
         return null; // Handle unknown image names
